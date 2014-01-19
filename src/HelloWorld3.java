8a5148b3619c72d5f5feed06b2f325e11b15df81
andretamm <andre.tamm@gmail.com>
2014-01-19 16:11:29 +0000

    Added code for second light sensor

--- src/HelloWorld3.java
+++ src/HelloWorld3.java
@@ -14,29 +14,30 @@ public class HelloWorld3 {
 	
     public static void main(String[] args) {
     	LightSensor lightS1 = new LightSensor(SensorPort.S1);  //defining the sensor port used
-    	LightSensor lightS2 = new LightSensor(SensorPort.S2);  //defining the sensor port used
+    	LightSensor lightS2 = new LightSensor(SensorPort.S4);  //defining the sensor port used
     	
     	
-        DifferentialPilot pilot  = new DifferentialPilot(2f, 4f, Motor.A, Motor.B, false); 
+        DifferentialPilot pilot  = new DifferentialPilot(1f, 2f, Motor.A, Motor.B, false); 
         LCD.drawString(Boolean.toString(lightS2.isFloodlightOn()), 0, 0);
         LCD.drawString("HelloWorld", 0, 0);
         LCD.drawInt(lightS2.getLightValue(), 0, 0); //print light sensor reading to screen
         
         while (true) {
         	
-            while (lightS2.getLightValue() < GreenWhiteThreshold){
+            while (lightS2.getLightValue() < GreenWhiteThreshold && lightS1.getLightValue() < GreenWhiteThreshold){
             	
-            	pilot.setTravelSpeed(4);
+            	pilot.setTravelSpeed(5);
             	pilot.forward();
-            	LCD.drawInt(lightS2.getLightValue(), 0, 0);          	
+            	
+//            	LCD.drawInt(lightS2.getLightValue(), 0, 0);
+            	System.out.println("1: " + lightS1.getLightValue() + " | 2: " + lightS2.getLightValue());
+            }          
+            
+            while (lightS2.getLightValue() >= GreenWhiteThreshold || lightS1.getLightValue() >= GreenWhiteThreshold){
+            	pilot.rotateLeft();
             }
             
-            while (lightS2.getLightValue() >= GreenWhiteThreshold){
-            	pilot.stop();
-            	pilot.rotateLeft();
-
-//            	pilot.rotate(90);
-            }
+            pilot.stop();
       } 
     	
     } 
