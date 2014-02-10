e04d9a377faa2e8a42973a98fae7c1b83e0501f7
andretamm <andre.tamm@gmail.com>
2014-02-10 18:49:04 +0000

    Changed added basic robot movement

--- commNXT/src/Robot.java
+++ commNXT/src/Robot.java
@@ -1,14 +1,19 @@
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.Sound;
+import lejos.robotics.MirrorMotor;
+import lejos.robotics.navigation.DifferentialPilot;
 
 
 public class Robot {
 
 	NXTRegulatedMotor kicker;
+	DifferentialPilot pilot;
 	
 	public void init() {
 		kicker = Motor.A;
+		pilot = new DifferentialPilot(56, 112, Motor.A, MirrorMotor.invertMotor(Motor.B), true);
+		pilot.setTravelSpeed(20);
 	}
 	
 	public void kick() {
@@ -19,22 +24,27 @@ public class Robot {
 	
 	public void forward() {
 		System.out.println("Going forward");
+		pilot.forward();
 	}
 	
 	public void backward() {
 		System.out.println("Going backward");
+		pilot.backward();
 	}
 	
 	public void turnCW() {
 		System.out.println("Turning clockwise");
+		pilot.rotateRight();
 	}
 	
 	public void turnCCW() {
 		System.out.println("Turning counterclockwise");
+		pilot.rotateLeft();
 	}
 	
 	public void chill() {
 		System.out.println("Chilling in place");
+		pilot.stop();
 	}
 	
 	public void fail() {
