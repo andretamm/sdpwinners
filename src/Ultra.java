686a677b258e8b70a17056bfd5d6b5da719a67a0
Marais Bourbon-Leftley <mbleftley@gmail.com>
2014-02-11 20:28:42 +0000

    Ultra.java
    - Refactored code to have 3 separate methods. Also note that I had to add a sleep after the grab, otherwise the sequential calls to the methods would have been way too fast.
    
    - Currently the speed is 600, if this is to fast, I can change that to a ratio for the kicker to catch it at the right time.

--- src/Ultra.java
+++ src/Ultra.java
@@ -1,5 +1,4 @@
 	
-import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
@@ -9,7 +8,7 @@ import lejos.robotics.navigation.DifferentialPilot;
 
 public class Ultra{
 	
-		public static int distanceToFloor = 25; //the distance to the floor from the current design
+		public static int distanceToFloor = 24; //the distance to the floor from the current design
 		public static int travelSpeed = 600; //the speed of the robot
 		static NXTRegulatedMotor grabber;
 		static DifferentialPilot pilot;
@@ -20,46 +19,43 @@ public class Ultra{
 	    	sonic = new UltrasonicSensor(SensorPort.S2);
 	    	pilot  = new DifferentialPilot(56, 112, Motor.A, Motor.B, false); 
 	    	grabber = Motor.C;
-	    	grabber.resetTachoCount();
-	    	grabber.rotateTo(0);
-	    	Button.ENTER.waitForPressAndRelease(); //Start the program
-	        	grabber.setSpeed(200);
-	        	grabber.setAcceleration(6000);
-	        	grabber.rotateTo(5);
-	        
-
-	        	//when the sensor detects a ball, it will go ape shit and run the grabBall method 
-	        	while(sonic.getDistance()>distanceToFloor){
-	        		pilot.setTravelSpeed(travelSpeed);
-	        		pilot.forward();
-	        		
-	            }
-	        	//when a ball is spotted, give it 100 millieseconds so that the grabber can get in position
-	        	Thread.sleep(300);
-	        	grabBall();
-	        
-	    }
-	    
-	    //this will grab the ball and kick it when called
-	    /*Note that these values used are in ratio with the robot speed,
-	    	by this I mean that the delay in grab time and sensoring time 
-	    	is fitted to the speed of 600.***/
-	    
-	    private static void grabBall() throws InterruptedException{
 	    	
 	    	pilot.setTravelSpeed(travelSpeed);
 	    	pilot.forward();
-        	//the grabber starts closed
-	    	grabber.setSpeed(600);
-        	grabber.setAcceleration(8000);
+	    	//When we are in line with the ball
+	    	activateSensor();
+	    	closeGrabber();
+	    	kickBall();
+	    		        
+	    }
+	    
+	    
+	    //sensor will be activated, this should be called when the robot is in line with the ball
+	    private static void activateSensor() throws InterruptedException{
+	    	
+	    	grabber.resetTachoCount();
+	    	grabber.rotateTo(0);
+	    	
+	    	while(sonic.getDistance()>distanceToFloor){
+        		pilot.setTravelSpeed(travelSpeed);
+        		pilot.forward();	
+            }
+	    	Thread.sleep(200);
+	    	
+	    }
+	    
+	    //called to grab the ball when the sensor sees the ball
+	    private static void closeGrabber() throws InterruptedException{
+	    	//the grabber closing speed 
+	    	grabber.setSpeed(800);
+        	grabber.setAcceleration(10000);
         	//close the grabber
-        	grabber.rotateTo(-10);
-        	Thread.sleep(100);
-        	
-        	//open the grabber
-        	pilot.stop(); //stop the robot before it makes the kick
-        	//This sleep is required for the ball to settle down, otherwise the kicker misses the ball
-        	Thread.sleep(100);
+        	grabber.rotateTo(-15);
+        	pilot.stop();
+        	Thread.sleep(1000);
+	    }
+
+		private static void kickBall() throws InterruptedException{
         	//full speed kick used from mile stone 2
         	grabber.setSpeed(1000);
         	grabber.setAcceleration(14000);
@@ -67,9 +63,10 @@ public class Ultra{
         	//reset the grabber to its original starting position
         	Thread.sleep(300);
         	grabber.rotateTo(-30);
-	    	
-	    	
-	    }
+			
+		}
+	    
+	   
 }
 	            
 	            
