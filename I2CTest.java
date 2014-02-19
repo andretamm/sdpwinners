ffe50e6d2986df1754691188b3dfa2a8c8dfd544
Marais Bourbon-Leftley <mbleftley@gmail.com>
2014-02-19 03:52:49 +0000

    Correct rotation for current design WITHOUT BRICK
    - Note that this is the correct values for rotating the current design WITHOUT a brick attached.
    
    - When I am in the labs sometime today, we will see where Bagdat can fit the brick and then fix these values accordingly.

--- I2CTest.java
+++ I2CTest.java
@@ -6,85 +6,130 @@ import lejos.nxt.SensorPort;
 
 
 public class I2CTest {
-
+	static I2CPort I2Cport;
+	static I2CSensor I2Csensor;
+	static byte forward; 
+	static byte backward; 
+	static byte off; 
+	static byte speed;
+	static byte EWrotSpeed;
+	static byte NSrotSpeed;
 	public static void main(String[] args) {
 
 
 		
-		I2CPort I2Cport; //Create a I2C port 
+		 //Create a I2C port 
 		I2Cport = SensorPort.S1; //Assign port 
-		I2CSensor I2Csensor = new I2CSensor(I2Cport, 0xB4, I2CPort.STANDARD_MODE, I2CSensor.TYPE_LOWSPEED_9V); 
+		I2Csensor = new I2CSensor(I2Cport, 0xB4, I2CPort.STANDARD_MODE, I2CSensor.TYPE_LOWSPEED_9V); 
 		
 		//Creates an I2CSensor 
-		byte forward = (byte)1; 
-		byte backward = (byte)2; 
-		byte off = (byte)0; 
-		byte speed = (byte)100; 
+		forward = (byte)1; 
+		backward = (byte)2; 
+		off = (byte)0; 
+		speed = (byte)200; 
+		
+		//to achieve rotation in place, the outside wheel have to catch up with the inside wheels.
+		EWrotSpeed = (byte)180; //the speed of the outside wheels
+		NSrotSpeed = (byte)(EWrotSpeed*0.78); //the speed of the inside wheels as a function of the speed of the outside wheels.
 		
 		//Facing forward towards the kicker, the wheels are programmed accordingly:
 		
 		/*              ** Forwards facing wheels **		*/
-		//0x01 (I2C - Port 1) - Wheel on the left of the robot, facing forwards.
-		//0x05 (I2C - Port 3) - Wheel on the right of the robot, facing forwards.
+		// Distance between wheel is 650mm
+		//0x01 (I2C - Port 1) - WEST Wheel.
+		//0x07 (I2C - Port 4) - EAST Wheel.
 		
 		/*				** Side Ways Facing Wheels **		*/
-		//0x03 (I2C - Port 2) - The front wheel, closest to the kicker.
-		//0x07 (I2C - Port 4) - The back wheel away from the kicker.
+		//Distance between wheels are 500mm
+		//0x03 (I2C - Port 2) - NORTH Wheel
+		//0x05 (I2C - Port 3) - SOUTH Wheel
 		 
 		
 		//Instructions for wheels
-		LCD.drawString("LEFT -> Rotation", 0, 0);
-		LCD.drawString("ENTER -> Backwards", 0, 2);
-		LCD.drawString("RIGHT -> Forwards", 0, 4);
-		LCD.drawString("ESCAPE -> STOP", 0, 6);
+		LCD.drawString("LEFT -> 90", 0, 0);
+		LCD.drawString("ENTER -> 180", 0, 2);
+		LCD.drawString("RIGHT -> 270", 0, 4);
+		LCD.drawString("ESCAPE -> 360", 0, 6);
 		
 		while (true){
 			int buttonID = Button.waitForAnyPress();
+			//button presses to test rotation
 			switch (buttonID) {
-			case Button.ID_LEFT:  //rotation movement
-				//left wheel facing forward
-				I2Csensor.sendData(0x01,backward); 
-				I2Csensor.sendData(0x02,speed); 
-				//right wheel facing forward
-				I2Csensor.sendData(0x05,backward); 
-				I2Csensor.sendData(0x06,speed); 
-				//front wheel at the kicker
-				I2Csensor.sendData(0x03,forward); 
-				I2Csensor.sendData(0x04,speed); 
-				//back wheel away from the kicker
-				I2Csensor.sendData(0x07,forward); 
-				I2Csensor.sendData(0x08,speed); 
+			case Button.ID_LEFT:  
+				try {
+					rotateTo(90);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
+			
 				break;
 			case Button.ID_ENTER:
-				//left wheel facing forward
-				I2Csensor.sendData(0x01,backward); 
-				I2Csensor.sendData(0x02,speed); 
-				//right wheel facing forward
-				I2Csensor.sendData(0x05,forward); 
-				I2Csensor.sendData(0x06,speed); 
+				try {
+					rotateTo(180);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
 				break;
 			case Button.ID_RIGHT: 
-				//left wheel facing forward
-				I2Csensor.sendData(0x01,forward); 
-				I2Csensor.sendData(0x02,speed); 
-				//right wheel facing forward
-				I2Csensor.sendData(0x05,backward); 
-				I2Csensor.sendData(0x06,speed); 
-				break;
+				try {
+					rotateTo(270);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
 			case Button.ID_ESCAPE:
-				I2Csensor.sendData(0x01,off); 
-				I2Csensor.sendData(0x02,off);
-				I2Csensor.sendData(0x03,off); 
-				I2Csensor.sendData(0x04,off); 
-				I2Csensor.sendData(0x05,off); 
-				I2Csensor.sendData(0x06,off); 
-				I2Csensor.sendData(0x07,off); 
-				I2Csensor.sendData(0x08,off); 
+				try {
+					rotateTo(360);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
 				break;
 		}
 		}
 			
 		}
+	
+	/*This function takes a number of degrees to rotate, as I have made it from scratch,
+	  it uses time to know how long it must rotate, according to the speed of the wheels*/
+	
+	public static void rotateTo(int degrees) throws InterruptedException{
+		
+		if(degrees < 0){
+			backward = (byte) -backward;
+			forward = (byte) -forward;
+			degrees = -degrees;
+		}
+		long degToTime = (long) Math.rint(degrees*12.65); // Based on the current speed of rotation
+		
+		//WEST Wheel
+		I2Csensor.sendData(0x01,backward); 
+		I2Csensor.sendData(0x02,EWrotSpeed); 
+		//EAST Wheel
+		I2Csensor.sendData(0x07,forward); 
+		I2Csensor.sendData(0x08,EWrotSpeed); 
+		//NORTH Wheel
+		I2Csensor.sendData(0x03,forward); 
+		I2Csensor.sendData(0x04,NSrotSpeed); 
+		//SOUTH Wheel
+		I2Csensor.sendData(0x05,backward); 
+		I2Csensor.sendData(0x06,NSrotSpeed); 
+		Thread.sleep(degToTime);
+		stop();
+	}
+	
+	public static void stop(){
+		I2Csensor.sendData(0x01,off); 
+		I2Csensor.sendData(0x02,off);
+		I2Csensor.sendData(0x03,off); 
+		I2Csensor.sendData(0x04,off); 
+		I2Csensor.sendData(0x05,off); 
+		I2Csensor.sendData(0x06,off); 
+		I2Csensor.sendData(0x07,off); 
+		I2Csensor.sendData(0x08,off); 
+	}
 		
 		
 
