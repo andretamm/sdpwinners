d1fb5c9a4a41e7847a5d0d7f81b3f946c9dfed0e
andretamm <andre.tamm@gmail.com>
2014-03-28 14:49:34 +0000

    Aiming the kicker is now non-blocking

--- commNXTHolonomic/src/Ultra360.java
+++ commNXTHolonomic/src/Ultra360.java
@@ -374,15 +374,15 @@ public class Ultra360 {
 	
 	//Rotate the front of the MaxBot
 	public void aimRight() {
-		rotator.rotateTo(-25); 		
+		rotator.rotateTo(-25, true); 		
 	}
 	public void aimLeft() {
-		rotator.rotateTo(25); 		
+		rotator.rotateTo(25, true); 		
 	}
 	
 	//This is to reset the rotator angle and align it straight again.
 	public void aimReset() {
-		rotator.rotateTo(0);
+		rotator.rotateTo(0, true);
 	}
 
 
