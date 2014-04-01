c9ae06b0d7d5b9d5e81c1b3d3a8fe7a3a29ec677
andretamm <andre.tamm@gmail.com>
2014-04-01 08:23:53 +0100

    Sleep on defender before passing is now unnecessary
    = faster passes yay!

--- 2014SDP1/behavior/finaldefender/DefenderGetInPositionForPass.java
+++ 2014SDP1/behavior/finaldefender/DefenderGetInPositionForPass.java
@@ -63,14 +63,6 @@ public class DefenderGetInPositionForPass extends GeneralBehavior {
 		// We're there!
 		Strategy.defenderReadyForPass = true;
 		
-		// Wait a bit to stabilise the robot
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-
-			e.printStackTrace();
-		}
-		
 	}
 
 	/** 
