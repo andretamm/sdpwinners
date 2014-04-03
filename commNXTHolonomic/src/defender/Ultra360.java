bd0ad095c64b288f26af6d6b815ebf08537bb019
Marais Bourbon-Leftley <mbleftley@gmail.com>
2014-04-03 04:04:10 +0100

    CloseGrabber has a new value to rotate to now.
    110 in use

--- commNXTHolonomic/src/defender/Ultra360.java
+++ commNXTHolonomic/src/defender/Ultra360.java
@@ -359,7 +359,7 @@ public class Ultra360 {
 	public void closeGrabber() {
 		grabber.setSpeed(400);
 	        grabber.setAcceleration(6000);
-	        grabber.rotateTo(105);
+	        grabber.rotateTo(110);
 	}
 	
 	public void openGrabber() {
